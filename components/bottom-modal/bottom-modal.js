c41813bd52bac6971d33e3b81236fa74ba29e9f0
vipjue <38695361+vipjue@users.noreply.github.com>
2022-08-18 00:47:53 +0800

    Update bottom-modal.js

--- components/bottom-modal/bottom-modal.js
+++ components/bottom-modal/bottom-modal.js
@@ -4,8 +4,9 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    
   },
-
+  
   /**
    * 组件的初始数据
    */
@@ -17,10 +18,6 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    onClose() {
-      this.setData({
-        modalShow: false,
-      })
-    },
+  
   }
 })
